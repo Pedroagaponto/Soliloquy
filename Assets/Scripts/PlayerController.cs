f84bedf57a22f8dfeae63e77ddfdc08b7880a5ab
auroralimin <auroralimin@hotmail.com>
2015-08-12 20:46:51 +1000

    Initial demo
    Adds all assets and configurations necessary to create a basic demo.
    This demo consists of 4 rooms and a moving character.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed = 6f;
+	
+	Vector3 movement;
+	Animator anim;
+	Rigidbody playerRigidbody;
+	
+	void Awake()
+	{
+		playerRigidbody = GetComponent <Rigidbody> ();
+		anim = GetComponent <Animator> ();
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxisRaw("Horizontal");
+		float v = Input.GetAxisRaw("Vertical");
+
+		Move(h, v);
+		Animating(h, v);
+	}
+	
+	void Move (float h, float v)
+	{
+		movement.Set(h, 0f, v);
+		movement = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y, Vector3.up) * movement;
+		movement = movement.normalized * speed * Time.deltaTime;
+		playerRigidbody.MovePosition(transform.position + movement);
+
+		/*
+		if(h != 0 || v != 0)
+		{
+			transform.rotation = Quaternion.LookRotation(movement);
+		}
+		*/
+	}
+
+	void Animating (float h, float v)
+	{
+		bool walking = h != 0f || v != 0f;
+		anim.SetBool ("IsWalking", walking);
+	}
+}
