83bee182ad4176f388178e299654728b7cd06956
Matthew Lai <s3431490@student.rmit.edu.au>
2015-09-16 15:11:32 +1000

    Controls fixed
    Character is controllable

---
+++ Assets/Scripts/RabbitAnimations.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RabbitAnimations : MonoBehaviour {
+	private Animator anim;
+	// Use this for initialization
+	void Awake()
+	{
+		anim = GetComponent <Animator> ();
+	}
+	void Start () {
+	
+	}
+	void FixedUpdate()
+	{
+		float h = Input.GetAxisRaw("Horizontal");
+		float v = Input.GetAxisRaw("Vertical");
+	
+		Animating(h, v);
+	}
+	void Animating (float h, float v)
+	{
+		bool walking = h != 0f || v != 0f;
+		anim.SetBool ("IsWalking", walking);
+	}
+}
