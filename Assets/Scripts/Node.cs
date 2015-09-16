e2acdd04124e91e73bd30ab852a702c720c285cd
auroralimin <auroralimin@hotmail.com>
2015-09-16 14:25:38 +1000

    Upgraded behaviour tree mechanism
    Upgrades and extends the behaviour tree mechanism so it can be
    more flexible and can be dowloaded from a twee file (that is
    generated from a twine project).
    
    For now there is a known bug when it comes to linking the nodes.
    During tests it can be seem that the right node is being correctly
    linked but the left node is not.

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -2,36 +2,19 @@
 using System.Collections;
 using System.Xml;
 using System.Xml.Serialization;
+using System.Collections.Generic;
 
-public class Node{
-	[XmlAttribute("id")]
-	public int id;
-	[XmlAttribute("nChild")]
-	public int nChild;
-	[XmlAttribute("pChild")]
-	public int pChild;
-	public string data;
-
-	private Node negativeChild;
-	private Node positiveChild;
-
-	public void setNegativeChild(Node negativeChild)
-	{
-		this.negativeChild = negativeChild;
-	}
-	
-	public void setPositiveChild(Node positiveChild)
-	{
-		this.positiveChild = positiveChild;
-	}
-
-	public Node getNegativeChild()
-	{
-		return negativeChild;
-	}
-	
-	public Node getPositiveChild()
-	{
-		return positiveChild;
-	}
+public class Node{	
+	public string Id { get; set; }
+	public string LChildId { get; set; }
+	public string RChildId { get; set; }
+	public string Narrator { get; set; }
+	public string Room { get; set; }
+	public int PlayerSpeed { get; set; }
+	public int TriggerType { get; set; }
+	public int WaitTime { get; set; }
+	public GameObject Obj { get; set; }
+	public Node LeftChild { get; set; }
+	public Node RightChild { get; set; }
+	public List<ObjectAnimation> objAnims = new List<ObjectAnimation>();
 }
