588c9543149a2d4871c51b97984fef06d3589e0e
auroralimin <auroralimin@hotmail.com>
2015-09-17 00:01:04 +1000

    Behaviour Tree that uses commands parsed from twee
    Implements methods on the BehaviourTree script that are used to
    communicate with other scripts. Thus, now is possible to use
    information collected from the twee file for the mechanism that
    trigger events involving other objects.
    
    Also, adds a "actionbutton" command that allows player to
    communicate with narrator through "spacebar" key.
    
    For now the tests showed that the event triggered by time and the
    event triggered by action button are working correctly. However,
    there are still known bugs when it comes to trigger an event by
    interacting with a specific object.

--- Assets/Scripts/TweeParser.cs
+++ Assets/Scripts/TweeParser.cs
@@ -29,13 +29,14 @@ public class TweeParser {
 		
 	private void Parse() {
 		int aux = 0;
+		bool error = false;
 		Node node = null;
 		StringBuilder buffer = new StringBuilder();
 		string[] lines;
 		string[] elements;
 		string line;
 
-		lines = tweeAsset.text.Split(new string[] {"\n"}, System.StringSplitOptions.None);
+		lines = tweeAsset.text.Replace("\r\n", "\n").Split('\n');
 		
 		for (int i = 0; i < lines.Length; i++) {
 			line = lines[i].ToLower();
@@ -45,121 +46,157 @@ public class TweeParser {
 				if (node != null) {
 					nodeList.Add(node);
 				}
-				node = new Node();
+				node = newNode();
 				node.Id = line.Substring(3);
 			} else {
 				elements = line.Split(new string[] {"-"}, System.StringSplitOptions.None);
 				if (elements[0].StartsWith(">narrator")) {
 					if (elements.Length < 2) {
-						Debug.Log("Invalid separator. Try to use \"-\" after \">narrator\" (node: " + node.Id + ", twee line: " + i + ")");
-						Application.Quit();
+						Debug.Log("Error in node \"" + node.Id + "\": Invalid separator on \">narrator\" command. Try to use \"-\" after \">narrator\".");
+						error = true;
+						continue;
 					}
 					else if (elements[1].Length == 0) {
-						Debug.Log("Missing narrator content (node: " + node.Id + ", line: " + i + ")");
-						Application.Quit();
+						Debug.Log("Error in node \"" + node.Id + "\": Missing narrator content.");
+						error = true;
+						continue;
 					}
 					node.Narrator = elements[1];
 				} else if (elements[0].StartsWith(">animation")) {
 					if (elements.Length < 3) {
-						Debug.Log("Invalid separator. Try to use \"-\" to separate contents (node: " + node.Id + ", twee line: " + i + ")");
-						Application.Quit();
+						Debug.Log("Error in node \"" + node.Id + "\": Invalid separator on \">animation\" command. Try to use \"-\" to separate contents.");
+						error = true;
+						continue;
 					} else if (elements[1].Length == 0) {
-						Debug.Log("Missing animation object (node: " + node.Id + ", line: " + i + ")");
-						Application.Quit();
+						Debug.Log("Error in node \"" + node.Id + "\": Missing animation object.");
+						error = true;
+						continue;
 					} else if (elements[2].Length == 0) {
-						Debug.Log("Missing animation trigger (node: " + node.Id + ", line: " + i + ")");
-						Application.Quit();
+						Debug.Log("Error in node \"" + node.Id + "\": Missing animation trigger.");
+						error = true;
+						continue;
 					}
 					node.objAnims.Add(new ObjectAnimation(elements[1], elements[2]));
 				} else if (line.StartsWith(">loadroom")) {
 					if (elements.Length < 3) {
-						Debug.Log("Invalid separator. Try to use \"-\" after \">loadroom\" (node: " + node.Id + ", twee line: " + i + ")");
-						Application.Quit();
+						Debug.Log("Error in node \"" + node.Id + "\": Invalid separator on \">loadroom\" command. Try to use \"-\" after \">loadroom\".");
+						error = true;
+						continue;
 					} else if (elements[1].Length == 0) {
-						Debug.Log("Missing loadroom content (node: " + node.Id + ", line: " + i + ")");
-						Application.Quit();
+						Debug.Log("Error in node \"" + node.Id + "\": Missing loadroom content.");
+						error = true;
+						continue;
 					}
 					node.Room = elements[1];				
 				} else if (line.StartsWith(">playerspeed")) {
 					if (elements.Length < 2) {
-						Debug.Log("Invalid separator. Try to use \"-\" after \">playerspeed\" (node: " + node.Id + ", twee line: " + i + ")");
-						Application.Quit();
+						Debug.Log("Error in node \"" + node.Id + "\": Invalid separator on \">playerspeed\" command. Try to use \"-\" after \">playerspeed\".");
+						error = true;
+						continue;
 					}
 					else if (elements[1].Length == 0) {
-						Debug.Log("Missing player speed (node: " + node.Id + ", line: " + i + ")");
-						Application.Quit();
+						Debug.Log("Error in node \"" + node.Id + "\": Missing player speed.");
+						error = true;
+						continue;
 					} else if (!int.TryParse(elements[1], out aux)) {
-						Debug.Log("Invalid playerspeed (line: " + i + ")");
-						Application.Quit();
+						Debug.Log("Error in node \"" + node.Id + "\": Invalid playerspeed.");
+						error = true;
+						continue;
 					}
 					node.PlayerSpeed = aux;
 				} else if (line.StartsWith("[[autointeract")) {
 					if (elements.Length < 3) {
-						Debug.Log("Invalid separator. Try to use \"-\" to separate contents (node: " + node.Id + ", twee line: " + i + ")");
-						Application.Quit();
+						Debug.Log("Error in node \"" + node.Id + "\": Invalid separator on \"autointeract\" command. Try to use \"-\" to separate contents.");
+						error = true;
+						continue;
 					} else if (elements[1].Length == 0) {
-						Debug.Log("Missing autointeract object (node: " + node.Id + ", line: " + i + ")");
-						Application.Quit();
+						Debug.Log("Error in node \"" + node.Id + "\": Missing autointeract object.");
+						error = true;
+						continue;
 					} else if (elements[2].Length == 0) {
-						Debug.Log("Missing autointeract next node (node: " + node.Id + ", line: " + i + ")");
-						Application.Quit();
+						Debug.Log("Error in node \"" + node.Id + "\": Missing autointeract next node.");
+						error = true;
+						continue;
 					}
-					node.TriggerType = (int) Trigger.autointeract;
-					node.Obj = GameObject.Find(elements[1]);
+					node.Triggers.Add((int) Trigger.autointeract);
+					node.TriggeredObjects.Add(GameObject.Find(elements[1]));
 					elements[2] = elements[2].Substring(1).Replace("]", string.Empty);
-					if (node.LChildId == null) {
-						node.LChildId = elements[2];
-					} else {
-						node.RChildId = elements[2];
-					}
+					node.ChildId.Add(elements[2]);
 				} else if (line.StartsWith("[[buttoninteract")) {
 					if (elements.Length < 3) {
-						Debug.Log("Invalid separator. Try to use \"-\" to separate contents (node: " + node.Id + ", twee line: " + i + ")");
-						Application.Quit();
+						Debug.Log("Error in node \"" + node.Id + "\": Invalid separator on \"buttoninteract\" command. Try to use \"-\" to separate contents.");
+						error = true;
+						continue;
 					} else if (elements[1].Length == 0) {
-						Debug.Log("Missing buttoninteract object (node: " + node.Id + ", line: " + i + ")");
-						Application.Quit();
+						Debug.Log("Error in node \"" + node.Id + "\": Missing buttoninteract object.");
+						error = true;
+						continue;
 					} else if (elements[2].Length == 0) {
-						Debug.Log("Missing buttoninteract next node (node: " + node.Id + ", line: " + i + ")");
-						Application.Quit();
+						Debug.Log("Error in node \"" + node.Id + "\": Missing buttoninteract next node.");
+						error = true;
+						continue;
 					}
-					node.TriggerType = (int) Trigger.buttoninteract;
-					node.Obj = GameObject.Find(elements[1]);
+					node.Triggers.Add((int) Trigger.buttoninteract);
+					node.TriggeredObjects.Add(GameObject.Find(elements[1]));
 					elements[2] = elements[2].Substring(1).Replace("]", string.Empty);
-					if (node.LChildId == null) {
-						node.LChildId = elements[2];
-					} else {
-						node.RChildId = elements[2];
-					}
+					node.ChildId.Add(elements[2]);
 				} else if (line.StartsWith("[[wait")) {
 					if (elements.Length < 3) {
-						Debug.Log("Invalid separator. Try to use \"-\" to separate contents (node: " + node.Id + ", twee line: " + i + ")");
-						Application.Quit();
+						Debug.Log("Error in node \"" + node.Id + "\": Invalid separator on \"wait\" command. Try to use \"-\" to separate contents.");
+						error = true;
+						continue;
 					} else if (elements[1].Length == 0) {
-						Debug.Log("Missing wait time (node: " + node.Id + ", line: " + i + ")");
-						Application.Quit();
+						Debug.Log("Error in node \"" + node.Id + "\": Missing wait time.");
+						error = true;
+						continue;
 					} else if (elements[2].Length == 0) {
-						Debug.Log("Missing wait next node (node: " + node.Id + ", line: " + i + ")");
-						Application.Quit();
+						Debug.Log("Error in node \"" + node.Id + "\": Missing wait next node.");
+						error = true;
+						continue;
 					} else if (!int.TryParse(elements[1], out aux)) {
-							Debug.Log("Invalid wait time (node: " + node.Id + ", line: " + i + ")");
-							Application.Quit();
+						Debug.Log("Error in node \"" + node.Id + "\": Invalid wait time.");
+						error = true;
+						continue;
 					}
-					node.TriggerType = (int) Trigger.buttoninteract;
+					node.Triggers.Add((int) Trigger.wait);
 					node.WaitTime = aux;
-
+					node.TriggeredObjects.Add(null);
 					elements[2] = elements[2].Substring(1).Replace("]", string.Empty);
-					if (node.LChildId == null) {
-						node.LChildId = elements[2];
-					} else {
-						node.RChildId = elements[2];
-					}
+					node.ChildId.Add(elements[2]);
+				} else if (line.StartsWith("[[actionbutton")) {
+					if (elements.Length < 2) {
+						Debug.Log("Error in node \"" + node.Id + "\": Invalid separator on \"actionbutton\" command. Try to use \"-\" to separate contents.");
+						error = true;
+						continue;
+					} else if (elements[1].Length == 0) {
+						Debug.Log("Error in node \"" + node.Id + "\": Missing actionbutton next node.");
+						error = true;
+						continue;
+					} 
+					node.Triggers.Add((int) Trigger.actionbutton);
+					node.TriggeredObjects.Add(GameObject.Find(elements[1]));
+					elements[1] = elements[1].Substring(1).Replace("]", string.Empty);
+					node.ChildId.Add(elements[1]);
 				} else if (line.StartsWith(">") || line.StartsWith("[[")) {
-					Debug.Log("Invalid tag (node: " + node.Id + ", line: " + i + ")");
-					Application.Quit();
+					Debug.Log("Error in node \"" + node.Id + "\": Invalid tag.");
+					error = true;
+						continue;
 				}
 			}
 		}
 		nodeList.Add(node);
+		if (error)
+			nodeList = null;
+	}
+
+	private Node newNode() {
+		Node node = new Node();
+		node.ChildId = new List<string>();
+		node.Triggers = new List<int> ();
+		node.objAnims = new List<ObjectAnimation>();
+		node.ChildNode = new List<Node>();
+		node.TriggeredObjects = new List<GameObject>();
+
+		return node;
 	}
 }
