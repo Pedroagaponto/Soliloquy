e75c0da8b70d18601a3be61b93623900c5149984
auroralimin <auroralimin@hotmail.com>
2015-09-30 12:05:57 +1000

    Cleaning project and room manager script
    Deletes some unecessary components in the project (as unused scenes)
    and implements a RoomManager script that make the rooms appear/
    disappear depending if the player is on it.

---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RoomManager : MonoBehaviour {
+	void Start () {
+		if (transform.name.Equals("FirstRoom/Floor")) {
+			ChangingAllRenderers(true);
+		} else {
+			ChangingAllRenderers(false);
+		}
+	}
+
+	void OnTriggerEnter(Collider other)	{
+		ChangingAllRenderers(true);
+	}
+
+	void OnTriggerExit(Collider other) {
+		ChangingAllRenderers(false);
+	}
+
+	void ChangingAllRenderers(bool renderState) 
+	{
+		List<Renderer> rendererList;
+		rendererList = new List<Renderer>(transform.parent.gameObject.GetComponentsInChildren<Renderer>());
+		foreach (Renderer render in rendererList)
+		{
+			render.enabled = renderState;
+		}
+	}
+}
