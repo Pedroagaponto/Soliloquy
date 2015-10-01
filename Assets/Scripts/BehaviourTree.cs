ed8ad5519090a01a5eba4903bcc21faf728d6b65
auroralimin <auroralimin@hotmail.com>
2015-10-01 13:25:53 +1000

    Sync audio, cubtitles and choices on 1st room
    Adds new audios and subtitles to the FirstRoom and changes the
    some time tiggers on Twine to make it more playable.

--- Assets/Scripts/BehaviourTree.cs
+++ Assets/Scripts/BehaviourTree.cs
@@ -8,21 +8,21 @@ public class BehaviourTree : MonoBehaviour {
 	public Node behaviour;
 
 	private NarratorController narrator;
-	private GameObject gameObject = null;
+	private List<GameObject> objList = new List<GameObject>();
 	private int oldTime, newTime;
 	private bool printed, positiveBehaviour;
 
 	public void TriggerNextChoice(int i) {
-		if (gameObject != null) {
-			gameObject.SendMessage("DeactivateTrigger");
-			gameObject = null;
+		foreach (GameObject obj in objList) {
+			obj.SendMessage("DeactivateTrigger");
 		}
+		objList = new List<GameObject>();
+
 		behaviour = behaviour.ChildNode[i];
 		oldTime = newTime = System.DateTime.Now.Second;
 		SetTriggers();
 		Debug.Log(behaviour.Narrator);
 		narrator.playDialog (behaviour.Id);
-
 		for (int j = 0; j < behaviour.objAnims.Count; j++) {
 			behaviour.objAnims[j].animateObject();
 		}
@@ -89,13 +89,15 @@ public class BehaviourTree : MonoBehaviour {
 	}
 
 	private void SetTriggers() {
+		GameObject obj;
 		if (behaviour == null || behaviour.Triggers == null)
 			return;
 		for (int i = 0; i < behaviour.Triggers.Count; i++) {
 			if (behaviour.TriggersNames[i] != null) {
-				gameObject = GameObject.Find(behaviour.TriggersNames[i]);
+				obj = GameObject.Find(behaviour.TriggersNames[i]);
+				objList.Add(obj);
 				Debug.Log(behaviour.TriggersNames[i]);
-				gameObject.SendMessage ("ActivateTrigger", i);
+				obj.SendMessage ("ActivateTrigger", i);
 			}
 		}
 	}
