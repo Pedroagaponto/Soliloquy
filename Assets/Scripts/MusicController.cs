23dcd9c602b81790339102f0b4e4a556caba9aae
Pedroagaponto <pedrohmpereira@gmail.com>
2015-10-14 10:00:20 +1100

    Adding the music triggers
    Now to call a music trigger can be done using the MusicController on the
    subtitle file like this:
    
    time |\call-Abstract/MusicController-PlayAudio-AUDIO_NAME
    00.67|\call-Abstract/MusicController-PlayAudio-happy
    
    The "happy" and "sad" are special cases, e.g. happy will one file from the
    folder Happy sequencially between calls.
    
    And for calling an specific file, just use the name of the file on
    Assets/Resources/Soundtrack

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class MusicController : MonoBehaviour {
+	private AudioSource musicAudio;
+	private AudioClip currentClip;
+	
+	private Queue<FileInfo> happyFiles;
+	private Queue<FileInfo> sadFiles;
+	private const string rootDir = "Assets/Resources/Soundtrack/";
+
+	// Use this for initialization
+	void Start () {
+		musicAudio = GetComponent<AudioSource> ();
+
+		DirectoryInfo dir = new DirectoryInfo(rootDir + "Happy/");
+		FileInfo[] info = dir.GetFiles("*.wav");
+		happyFiles = new Queue<FileInfo> (info);
+		
+		dir = new DirectoryInfo(rootDir + "Sad/");
+		info = dir.GetFiles("*.wav");
+		sadFiles = new Queue<FileInfo> (info);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void PlayAudio (string filename) {
+		if(filename.Contains("happy")){
+			FileInfo f = happyFiles.Dequeue();
+			happyFiles.Enqueue(f);
+			filename = "Happy/"+Path.GetFileNameWithoutExtension(f.Name);
+		}
+		else if(filename.Contains("sad")){
+			FileInfo f = sadFiles.Dequeue();
+			sadFiles.Enqueue(f);
+			filename = "Sad/"+Path.GetFileNameWithoutExtension(f.Name);
+		}
+
+		currentClip = Resources.Load ("Soundtrack/" + filename) as AudioClip;
+
+		
+		if (currentClip != null) {
+			musicAudio.clip = currentClip;
+			musicAudio.Play ();
+		}
+	}
+
+}
