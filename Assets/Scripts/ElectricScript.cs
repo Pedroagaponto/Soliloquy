a488dbe9c1b94bea0fb0a98c40809e115303690b
Matthew Lai <s3431490@student.rmit.edu.au>
2015-10-13 14:02:25 +1100

    sounds and electric stuff

---
+++ Assets/Scripts/ElectricScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElectricScript : MonoBehaviour {
+	public GameObject electricStuff;
+	public AudioClip myClip;
+	// Use this for initialization
+	void Electrocute () {
+		electricStuff.SetActive(true);
+		Debug.Log ("Working");
+	}
+	
+	// Update is called once per frame
+	void noElectroctue () {
+		electricStuff.SetActive(false);
+	}
+
+	void electricSound(){
+		GetComponent<AudioSource>().PlayOneShot(myClip);
+}
+}
