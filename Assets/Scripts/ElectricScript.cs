dab7751fcbf6cb6abc626cc922fc6a2a79c1dd61
Matthew Lai <s3431490@student.rmit.edu.au>
2015-10-14 13:55:43 +1100

    Electric Fixed!!!

--- Assets/Scripts/ElectricScript.cs
+++ Assets/Scripts/ElectricScript.cs
@@ -7,11 +7,14 @@ public class ElectricScript : MonoBehaviour {
 
 	void Electrocute () {
 		electricStuff.SetActive(true);
+		//electricStuff.GetComponent<Animator>().enabled = true;
 		Debug.Log ("Working");
 	}
 
-	void noElectroctue () {
+	void noElectroctute () {
 		electricStuff.SetActive(false);
+		//electricStuff.GetComponent<Animator>().enabled = false;
+		Debug.Log ("No Ele");
 	}
 
 	void electricSound(){
