c65d5ee9feca0f0e331778370abace5089ecdd80
Matthew Lai <s3431490@student.rmit.edu.au>
2015-10-22 16:20:42 +1100

    final fixes

--- Assets/Scripts/ElectricScript.cs
+++ Assets/Scripts/ElectricScript.cs
@@ -4,6 +4,20 @@ using System.Collections;
 public class ElectricScript : MonoBehaviour {
 	public GameObject electricStuff;
 	public AudioClip myClip;
+	private Animator anim;
+
+	void Awake()
+	{
+		anim = GetComponent <Animator> ();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Lamp") {
+			anim.SetTrigger("Zap");
+		Debug.Log("yas");
+		}
+	}
 
 	void Electrocute () {
 		electricStuff.SetActive(true);
